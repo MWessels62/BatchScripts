b94ab843c03182bbbdc19d3c19a764a5eec1c7d4
Michael Wessels <MWessels62@users.noreply.github.com>
2019-09-12 16:25:22 +0200

    Added functionality to archive my Downloads folder

--- StartupScript.bat
+++ StartupScript.bat
@@ -1,9 +1,14 @@
 @echo off
 
-title My very first batch script
+title Startup script
+rem This file runs every time the computer boots up 
 
+rem The following opens up these websites in my default browser
 start "" https://mail.google.com/mail/u/0/#inbox
 start "" https://github.com/MWessels62
 
-
+rem The login user, date and time are logged
 echo Logged time = %USERNAME% - %time% - %date%>> LogFile.txt
+
+rem The following archives my Downloads folder contents to a backup drive
+move C:\Users\User\Downloads\*.* F:\DownloadsArchive
